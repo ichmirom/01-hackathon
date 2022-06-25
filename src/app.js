137870bdd143ee0d4375788de427faca63a4ae7b
Roman Burkeev <raburkeev@gmail.com>
2022-06-25 22:42:19 +0300

    add sound module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import './styles.css'
 import {ContextMenu} from "./menu";
 import {BackgroundModule} from "./modules/background.module";
+import {SoundModule} from "./modules/sound.module";
 
 const contextMenu = new ContextMenu('#menu')
 const backgroundModule = new BackgroundModule('background-module', 'Изменить цвет заднего фона')
+const soundModule = new SoundModule('sound-module', 'Воспроизвести случайный звук')
 
 contextMenu.add(backgroundModule)
+contextMenu.add(soundModule)
