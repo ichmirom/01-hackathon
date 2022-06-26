61a4cf13123e8800bead76c730a588618265bf62
Иван Петров <fatevector@yandex.ru>
2022-06-27 00:37:41 +0300

    Fixed click-module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,16 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
-    #score
     constructor(type, text) {
         super(type, text);
         this.div = document.createElement('div')
         this.div.className = 'container_clicker'
         this.div.textContent = 'Анализатор подсчета кликов'
+        this.div.style.paddingTop = '10px'
         this.form = document.createElement('form')
         this.form.className = 'container_clicker__form'
         this.timeForClick = document.createElement('input')
-        // this.timeForClick.name = 'timer'
         this.timeForClick.type = 'text'
         this.timeForClick.placeholder = 'Введите секунды'
         this.timeForClick.className = 'container_clicker__time'
@@ -18,65 +17,50 @@ export class ClicksModule extends Module {
         this.btn_start.type = 'submit'
         this.btn_start.value = 'Начать подсчет кликов'
         this.btn_start.className = 'container_clicker__button'
-        this.#score = document.createElement('div')
-        this.#score.className = 'container_clicker__score'
+        this.score = document.createElement('div')
+        this.score.className = 'container_clicker__score'
         this.clicks = 0
-        console.log(this.#score)
+        this.exit = document.createElement('button')
+        this.exit.className = 'container_clicker_exit_button'
+        this.exit.textContent = 'Закрыть окно'
+        this.btn_start.addEventListener('click', event => this.clickOnButton(event))
+        this.exit.addEventListener('click', this.closeWindow.bind(this))
+        this.form.append(this.timeForClick, this.btn_start)
+        this.div.append(this.form, this.score, this.exit)
+    }
+
+    closeWindow() {
+        this.div.remove()
+        this.clicks = 0
     }
 
     startCount() {
         this.clicks++
-        console.log(this.#score)
-        this.#score.textContent = `Вы сделали кликов: ${this.clicks}! `
+        const score = document.querySelector('.container_clicker__score')
+        this.score.textContent = `Вы сделали кликов: ${this.clicks}! `
     }
 
     clickOnButton(event) {
         event.preventDefault()
-        console.log(this.timeForClick)
         let value = Number(this.timeForClick.value)
-        if(value !== 0 && !isNaN(value)) {
+        if (value !== 0 && !isNaN(value)) {
             this.clicks = 0
             this.btn_start.setAttribute('hidden', '')
-
-            document.addEventListener('click', this.startCount)
+            const bindedStartCount = this.startCount.bind(this)
+            document.addEventListener('click', bindedStartCount)
             setTimeout(() => {
                 this.btn_start.removeAttribute('hidden')
-                document.removeEventListener('click', this.startCount)
+                document.removeEventListener('click', bindedStartCount)
             }, value * 1000)
         } else {
             event.preventDefault()
-            this.#score.textContent = 'Поле не дожно быть пустым и содержать буквы'
+            this.score.textContent = 'Поле не дожно быть пустым и содержать буквы'
         }
     }
     trigger() {
         document.body.append(this.div)
-        this.div.append(this.form,  this.#score)
-        this.form.append(this.timeForClick, this.btn_start)
-
-        // let clicks = 0
-
-        this.btn_start.addEventListener('click', event => this.clickOnButton(event))
-        console.log(this.timeForClick)
-        // function clickOnButton(event) {
-        //     event.preventDefault()
-        //     console.log(this.timeForClick)
-        //     let value = Number(this.timeForClick.value)
-        //     if(value !== 0 && !isNaN(value)) {
-        //         clicks = 0
-        //         this.btn_start.setAttribute('hidden', '')
-        //         function startCount() {
-        //             clicks++
-        //             this.score.textContent = `Вы сделали кликов: ${clicks}! `
-        //         }
-        //         document.addEventListener('click', startCount)
-        //         setTimeout(() => {
-        //             this.btn_start.removeAttribute('hidden')
-        //             document.removeEventListener('click', startCount)
-        //         }, `${value * 1000}`)
-        //     } else {
-        //         event.preventDefault()
-        //         this.score.textContent = 'Поле не дожно быть пустым и содержать буквы'
-        //     }
-        // }
+        this.clicks = 0
+        this.score.textContent = ''
+        this.timeForClick.value = ''
     }
 }
