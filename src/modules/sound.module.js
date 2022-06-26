9b5bed225a78fa9ed29befb5635efa48c6719c90
Roman Burkeev <raburkeev@gmail.com>
2022-06-26 17:04:14 +0300

    final fix sound-module

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -2,15 +2,17 @@ import {Module} from "../core/module";
 import {random} from "../utils";
 
 export class SoundModule extends Module {
+    #currentSound
+
     constructor(type, text) {
         super(type, text)
-        this.currentSound = new Audio()
-        this.currentSound.volume = 0.3
+        this.#currentSound = new Audio()
+        this.#currentSound.volume = 0.3
     }
 
     trigger() {
-        this.currentSound.pause()
-        this.currentSound.src = `../src/sounds/sound${random(1, 15)}.mp3`
-        this.currentSound.play()
+        this.#currentSound.pause()
+        this.#currentSound.src = `../src/sounds/sound${random(1, 15)}.mp3`
+        this.#currentSound.play()
     }
 }
