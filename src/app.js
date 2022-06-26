38e2df9398caeb1988d0c15c753909ad4362b86f
rasul <rasulc558@gmail.com>
2022-06-26 04:57:29 +0400

    add messages module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-import './styles.css'
-import {ContextMenu} from "./menu";
-import {BackgroundModule} from "./modules/background.module";
+import './styles.css';
+import { ContextMenu } from './menu';
+import { BackgroundModule } from './modules/background.module';
+import { Messages } from './modules/messages.module';
 
-const contextMenu = new ContextMenu('#menu')
-const backgroundModule = new BackgroundModule('background-module', 'Изменить цвет заднего фона')
+const contextMenu = new ContextMenu('#menu');
+const backgroundModule = new BackgroundModule(
+  'background-module',
+  'Изменить цвет заднего фона'
+);
+const messages = new Messages('messages-module', 'Показать мотивашку');
 
-contextMenu.add(backgroundModule)
+contextMenu.add(backgroundModule);
+contextMenu.add(messages);
